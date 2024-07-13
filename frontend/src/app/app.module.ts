b2efe72509b4255568942ec708d1c387c3149347
sumeetkadam <sumeetkadam22@gmail.com>
2024-07-13 19:02:30 +0530

    initial commit

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -3,14 +3,27 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/partials/header/header.component';
+import { HomeComponent } from './components/pages/home/home.component';
+import { SearchComponent } from './components/partials/search/search.component';
+import { TagsComponent } from './components/partials/tags/tags.component';
+// import { NgxStarRatingModule } from 'ngx-star-rating';
+// import { StarRatingModule } from 'angular-star-rating';
+// import { RatingModule, StarRatingComponent } from 'ng-starrating';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    HomeComponent,
+    SearchComponent,
+    TagsComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    // NgxStarRatingModule,
+    // StarRatingModule.forRoot()
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ frontend/src/app/components/pages/home/home.component.ts
@@ -0,0 +1,25 @@
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { FoodService } from 'src/app/services/food.service';
+import { Food } from 'src/app/shared/models/Food';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.css'],
+})
+export class HomeComponent implements OnInit {
+  foods: Food[] = [];
+  constructor(private foodService: FoodService, activateRoute: ActivatedRoute) {
+    activateRoute.params.subscribe((params)=>{
+      if(params.searchTerm)
+      this.foods=this.foodService.getAllFoodBySearchTerm(params.searchTerm);
+    else if(params.tag)
+    this.foods=this.foodService.getAllFoodByTag(params.tag);
+  else
+    this.foods=foodService.getAll();
+    })
+  }
+
+  ngOnInit(): void {}
+}
